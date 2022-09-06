e3555ea7b1abc5c9477b6a38f313303f36b22b69
mkartyshov <mkartyshov@proton.me>
2022-09-06 20:57:11 +0300

    update

--- IndependenceDayCard/app/src/main/java/com/example/independencedaycard/MainActivity.kt
+++ IndependenceDayCard/app/src/main/java/com/example/independencedaycard/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.independencedaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.independencedaycard.ui.theme.IndependenceDayCardTheme
 
@@ -19,10 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             IndependenceDayCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+                Surface(color = MaterialTheme.colors.background) {
+                    GreetingWithImage(getString(R.string.greetings_text), getString(R.string.signature_text))
                 }
             }
         }
@@ -31,20 +35,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingWithText(message: String, from: String) {
-    Text(
-        text = message,
-        fontSize = 30.sp
-    )
-    Text(
-        text = from,
-        fontSize = 20.sp
-    )
+    Column {
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 13.dp, top = 13.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 13.dp, top = 13.dp)
+
+        )
+    }
 }
 
-@Preview(showBackground = false)
+@Composable
+fun GreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.bg)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        GreetingWithText(message = message, from = from)
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun IndependencePreview() {
     IndependenceDayCardTheme {
-        GreetingWithText("Happy Independence Day!", "- from Mikhail")
+        GreetingWithImage("Happy Independence Day!", "- from ZSU")
     }
 }
