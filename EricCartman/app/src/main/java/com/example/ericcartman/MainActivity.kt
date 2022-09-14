6c0da6b512af64c5ce7913abbb962a89372e1eb5
mkartyshov <mkartyshov@proton.me>
2022-09-14 16:22:21 +0300

    upd

--- EricCartman/app/src/main/java/com/example/ericcartman/MainActivity.kt
+++ EricCartman/app/src/main/java/com/example/ericcartman/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.ericcartman
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ericcartman.ui.theme.EricCartmanTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +27,9 @@ class MainActivity : ComponentActivity() {
             EricCartmanTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BiographyApp()
                 }
             }
         }
@@ -30,14 +37,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BiographyApp() {
+    BiographyWithImage(
+        name = stringResource(R.string.name_text),
+        info = stringResource(R.string.info_text),
+        bio = stringResource(R.string.bio_text),
+        image = painterResource(R.drawable.header)
+    )
+}
+
+@Composable
+private fun BiographyWithImage(name: String, info: String, bio: String, image: Painter) {
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = name,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = info,
+            textAlign = TextAlign.Justify,
+            fontSize = 25.sp,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = bio,
+            textAlign = TextAlign.Justify,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EricCartmanTheme {
-        Greeting("Android")
+        BiographyApp()
     }
 }
