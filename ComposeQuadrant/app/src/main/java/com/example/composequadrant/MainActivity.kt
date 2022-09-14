4a57d64155205dc6e9bf9d4ccacfedb1657d0f16
mkartyshov <mkartyshov@proton.me>
2022-09-14 17:12:17 +0300

    updated

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,11 +30,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeQuadrant()
                 }
             }
         }
@@ -32,21 +39,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Quadrant() {
-    Column() {
-        Row() {
-
+fun ComposeQuadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            QuadrantBlock(
+                title = stringResource(R.string.quadrant1name),
+                description = stringResource(R.string.quadrant1text),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantBlock(
+                title = stringResource(R.string.quadrant2name),
+                description = stringResource(R.string.quadrant2text),
+                backgroundColor = Color.Blue,
+                modifier = Modifier.weight(1f)
+            )
         }
-        Row() {
-
+        Row(Modifier.weight(1f)) {
+            QuadrantBlock(
+                title = stringResource(R.string.quadrant3name),
+                description = stringResource(R.string.quadrant3text),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantBlock(
+                title = stringResource(R.string.quadrant4name),
+                description = stringResource(R.string.quadrant4text),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
         }
     }
+}
+
+@Composable
+fun QuadrantBlock(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+   Column(
+       modifier = modifier
+           .fillMaxSize()
+           .background(backgroundColor)
+           .padding(16.dp),
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center
+   ) {
+       Text(
+           text = title,
+           fontWeight = FontWeight.Bold,
+           modifier = Modifier.padding(bottom = 16.dp)
+       )
+       Text(
+           text = description,
+           textAlign = TextAlign.Justify
+       )
+   }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrant()
     }
 }
